b83f4eb858fdb9bef18f19e484079a5545b530a9
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2020-10-18 12:55:43 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -1,15 +1,17 @@
 class dustbin{
     constructor(x,y,width,height){
-        this.x
-        this.y
-        this.width
-        this.height
+        var options={
+            isStatic:true
+        }
+        this.width=width,
+        this.height=height,
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        World.add(world,this.body)
     }
     display(){
-        push()
-        translate(this.body.position.x,this.body.position.y)
+        var pos =this.body.position
         rectMode(CENTER)
-        rect(0,0,this.width,this.height)
-        pop()
+        fill(255);
+        rect(pos.x,pos.y,this.width,this.height)
     }
 }
