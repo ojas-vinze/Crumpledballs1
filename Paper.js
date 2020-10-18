860cce164d61b30198276bf1e6b20e5df8813b48
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2020-10-18 12:57:03 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -2,24 +2,19 @@ class paper{
     constructor(x,y,r){
         var options = {
             isStatic:false,
-            restitution:0.3,
+            restitution:0.8,
             friction:0.5,
             density:1.2
         }
-
-        this.x=x
-        this.y=y
         this.r=r
-        this.body = Bodies.circle(this.x,this.y,this.r/2,options)
+        this.body = Bodies.circle(x,y,this.r,options)
+        World.add(world,this.body)
 
     }
-    dispaly(){
+    display(){
         var ppos = this.body.position
-        push()
-        translate(ppos.x,ppos.y)
-        rectMoade(CENTER)
-        fill(255,0,255)
-        ellipse(0,0,this.r,this.r)
-        pop()
+        fill("magenta")
+        ellipseMode(RADIUS)
+        ellipse(ppos.x,ppos.y,this.r,this.r)
     }
 }
